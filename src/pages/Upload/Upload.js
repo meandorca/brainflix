65ebb145964e9e4fb48fa0a4105e7fe94588906b
O&A <av.begreat@gmail.com>
2022-11-20 09:20:28 -0500

    axios changes

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -2,14 +2,43 @@ import './Upload.scss'
 import blue from '../../assets/Images/Upload-video-preview.jpg'
 import publish from '../../assets/Icons/publish.svg'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 
 function Upload() {
+    const [formData,setFormData] = useState({
+        "id": "",
+        "title": "",
+        "channel": "Red Cow",
+        "image": "/images/nicki-thick.jpg",
+        "description": "",
+        "views": "0",
+        "likes": "0",
+        "duration": "4:01",
+        "video": "https://project-2-api.herokuapp.com/stream",
+        "timestamp": "",
+        "comments": [
+        ]
+      },
+    )
 
     const navigate = useNavigate();
-    const navigateHome = () => {
-        alert("Uploaded!")
-        navigate('/');
+    const navigateHome = (e) => {
+        e.preventDefault() 
+
+        axios.post('http://localhost:8000/videos', formData)
+        .then((response => {
+            alert("Uploaded!")
+            navigate('/');
+        })).catch((error) => {
+            console.log(error)
+        })
     };
+
+    function dataChange(e) {
+        setFormData ({...formData,[e.target.name]:e.target.value})
+
+    }
 
 
     return (
@@ -27,11 +56,11 @@ function Upload() {
                 <div className='upload__desk'>
                 <p className='upload__content--name'>TITLE YOUR VIDEO</p>
                 <label>
-                    <input type="text" className='upload__input' placeholder='Add a title to your video' name="title" />
+                    <input type="text" onChange={dataChange} value={formData.title} className='upload__input' placeholder='Add a title to your video' name="title" />
                 </label>
                 <p className='upload__content--name'>ADD A VIDEO DESCRIPTION</p>
                 <label>
-                    <input type="description" className='upload__input upload__description' placeholder='Add a description to your video' name="description" />
+                    <input type="description" onChange={dataChange} value={formData.description} className='upload__input upload__description' placeholder='Add a description to your video' name="description" />
                 </label>
                 </div>
 
